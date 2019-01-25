e5131f1ee1b9b003fb6fdd093798bcd2310b9a56
edloaa <edloaa@gmail.com>
2019-01-25 18:18:46 +0100

    Simple C++ template

---
+++ templates/.templates/cpp.cpp
@@ -0,0 +1,27 @@
+/*
+File: filename
+Author: name
+Email: name@email.com
+Description: description
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double PI = 3.1415926535;
+    char a = 'a';
+    bool isCorrect = true;
+    int i;
+    float x = 3.3;
+    char message[] = "Hello world!\n";
+
+    cout << message;
+
+    for (i = 0; i < 1e2; ++i) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
+
