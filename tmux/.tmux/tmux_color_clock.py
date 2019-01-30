942620b79c2e7e017de13c3de78826cc6f8804ba
edloaa <edloaa@gmail.com>
2019-01-30 21:03:41 +0100

    Smother transition between tmux clock colours
    * Update tmux colour every minute

--- tmux/.tmux/tmux_color_clock.py
+++ tmux/.tmux/tmux_color_clock.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# (C) edloaa
+# 2019-01-30
 
 import time
 import datetime
 import os
+from colour import Color, web2hex, rgb2hex, hex2rgb
+
+
+# TODO:
+# - don't allow duplicates in gradient colour list
+# - make sure that always n_desired points are returned
 
 
 TMUX_SOCKET_NAME="default"
@@ -12,55 +20,62 @@ TMUX_CHANGE_COLOR_CMD='tmux -L {:s} setw -g clock-mode-colour "{:s}"'
 
 
 def set_tmux_clock_colour(colour_id):
+    """
+    Send command to TMUX sessions to update the clock colour.
+
+    Args:
+        :colour_id: (str) name of colour (valid TMUX format)
+    """
+
     cmd = TMUX_CHANGE_COLOR_CMD.format(TMUX_SOCKET_NAME, colour_id)
     print(cmd)
     os.system(cmd)
 
 
-def quasi_random_colours():
-    colour_list = range(20, 256)
+def gen_colour_gradient(colour_supports, n_desired):
+    """
+    Return colour gradient in hex format.
 
-    while True:
-        for colour in colour_list:
-            set_tmux_clock_colour("colour{:d}".format(colour))
-            time.sleep(60)
+    Args:
+        :colour_supports: (str-list) list of colour support points
+        :n_desired: (int) number of colours in gradient
+
+    Returns:
+        :gradient: (str-list) gradient colour list
+    """
+
+    colour_list = [Color(colour) for colour in colour_supports]
+    n_list = len(colour_list)
+    n_per_pair = int(n_desired/(n_list-1))
+    gradient = []
+
+    for i in range(n_list - 1):
+        c1 = colour_list[i]
+        c2 = colour_list[i+1]
+        gradient += list(c1.range_to(c2.hex, n_per_pair))
+
+    for i, colour in enumerate(gradient):
+        gradient[i] = Color(colour).hex_l
+
+    return list(gradient)
 
 
 def colour_gradient():
-    colour_gradient = {
-        0: '#3eb36e',
-        1: '#00b194',
-        2: '#00adaf',
-        3: '#0090ba',
-        4: '#006fc4',
-        5: '#0048bd',
-        6: '#190097',
-        7: '#000ac5',
-        8: '#005be9',
-        9: '#0080dd',
-        10: '#009cbb',
-        11: '#00b4a1',
-        12: '#19b37d',
-        13: '#4baf4e',
-        14: '#75a700',
-        15: '#8c8700',
-        16: '#986500',
-        17: '#9c3f00',
-        18: '#970000',
-        19: '#b70000',
-        20: '#af4e00',
-        21: '#9f7500',
-        22: '#8a9500',
-        23: '#70b040',
-        }
+    """
+    Make TMUX change colour over the course of the day.
+    """
+
+    gradient = gen_colour_gradient(["blue", "green", "yellow", "red"], 24*60)
 
     while True:
         hour = datetime.datetime.now().hour
-        set_tmux_clock_colour(colour_gradient[hour])
+        minute = datetime.datetime.now().minute
+        dayminute = hour*60 + minute
+
+        set_tmux_clock_colour(str(gradient[dayminute]))
         time.sleep(60)
 
 
 if __name__ == '__main__':
-    # quasi_random_colours()
     colour_gradient()
 
