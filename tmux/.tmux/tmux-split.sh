34d24bbbacea0191ac42e5a6d3365dd58161188e
edloaa <edloaa@gmail.com>
2019-02-06 22:07:37 +0100

    Updated tmux standard window splits

--- tmux/.tmux/tmux-split.sh
+++ tmux/.tmux/tmux-split.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 tmux new-session \; \
-    split-window -h -p 50 \; \
+    split-window -h -p 60 \; \
     select-pane -t 1 \; \
-    split-window -v -p 50 \; \
-    split-window -v -p 30 \; \
-    clock-mode -t 3 \; \
-    # select-pane -t 5 \; \
+    split-window -v -p 5 \; \
+    clock-mode -t 2 \; \
+    select-pane -t 3 \; \
+    # split-window -v -p 50 \; \
     # send-keys 'vim' C-m \; \
 
