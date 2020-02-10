e56ef4ec9a4316a4af384164d33c6742dd29da0e
edloaa <edloaa@gmail.com>
2020-02-10 18:44:54 +0100

    Update Powershell settings

--- _windows/powershell_settings.ps1
+++ _windows/powershell_settings.ps1
@@ -5,11 +5,22 @@ Write-Host
 
 set HOME="$HOMEDRIVE$HOMEPATH"
 Set-Location $HOME
-cd $HOME
+
+# Shortcuts
+$dotfiles = "$HOMEDRIVE/myconf/dotfiles"
+$desktop = "$HOME/Desktop"
+$dev = "$HOME/Desktop/git_tracking"
 
 # Aliases
 Set-Alias -Name g -Value git
 Set-Alias -Name ll -Value ls
+Set-Alias -Name l -Value ls
 
 Function OpenFileBrowserHere {explorer .}
 Set-Alias -Name t -Value OpenFileBrowserHere
+
+# Source the settings file
+Function SourceSettings {. $HOMEDRIVE/myconf/dotfiles/_windows/powershell_settings.ps1}
+Set-Alias -Name source -Value SourceSettings
+
+cd $HOME
