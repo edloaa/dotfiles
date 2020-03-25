ffcd57f77dc323441a514a29aa996ca92be0b8dc
edloaa <edloaa@gmail.com>
2020-03-25 18:11:40 +0100

    Jaeb8aechohz5Eghunieg5Phei7ohd2oiheuf3aiz2oh9eeZei

--- other_customs/scripts/fake_activity.py
+++ other_customs/scripts/fake_activity.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+
 import pyautogui as pg
 
 
 def move_mouse_left_right(px=1, pause=55):
+    pg.FAILSAFE = False if pause > 1 else True
     while True:
         pg.move(px, 0, pause=pause)
         px *= -1
 
 
 if __name__ == '__main__':
-    move_mouse_left_right()
+    try:
+        move_mouse_left_right()
+    except KeyboardInterrupt:
+        sys.exit(1)
